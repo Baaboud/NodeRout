8646d00d50d812f86fb91611dbe5431b0ff68ffa
Baaboud <Baaboud.dev@gmail.com>
2022-02-16 14:42:48 +0300

    Fix Rout Cases

--- index.js
+++ index.js
@@ -3,38 +3,47 @@ const fs = require("fs");
 const url = require("url");
 
 server = http.createServer( function(req, res) {
-    var u = url.parse(req.url, true);
-    // let p= u.pathname;
+    // URL full path
+    let u = url.parse(req.url, true);
+    // URL query
     let q= u.query;
+    // if the query is not empty
     if(Object.keys(q).length !== 0){
+        // if the role is admin linke: home.html?role:admin
         if(q.role=="admin"){
             fs.readFile('admin.html', function(err, page) {
                 res.writeHead(200, {'Content-Type': 'text/html'});
                 res.write(page);
                 res.end();
             });
-        }else{
+        }
+        else {
             fs.readFile('login.html', function(err, page) {
                 res.writeHead(403, {'Content-Type': 'text/html'});
                 res.write(page);
                 res.end();
             });
         }
-    }else
-    {
+    }
+    // if there is no query in url
+    else {
+        // defalut home page rout case
         if( req.url== "/home.html" || req.url== "/"){
             fs.readFile('home.html', function(err, page) {
                 res.writeHead(200, {'Content-Type': 'text/html'});
                 res.write(page);
                 res.end();
             });
-        }else if( req.url=="/blog.html"){
+        }
+        // blog page rout
+        else if( req.url=="/blog.html"){
             fs.readFile('blog.html', function(err, page) {
                 res.writeHead(200, {'Content-Type': 'text/html'});
                 res.write(page);
                 res.end();
             });
         }
+        // other cases
         else{
             fs.readFile('404.html', function(err, page) {
                 res.writeHead(404, {'Content-Type': 'text/html'});
